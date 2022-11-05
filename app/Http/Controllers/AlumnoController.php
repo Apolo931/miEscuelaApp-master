3b09c29233c5f306197ccf2df351b7f9293f44aa
Apolo931 <apolo931@outlook.es>
2022-11-05 12:35:50 -0700

    Todo de una

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Alumno;
+
+class AlumnoController extends Controller
+{
+    function consultar(){
+    
+     
+    $alumnos = Alumno::paginate(3);
+    
+    return view('alumno.alumnos', compact('alumnos'));
+    }
+
+    function registrar(){
+        return view('alumno.registrar');
+    }
+
+    function guardar(Request $datos){
+        $alumno = Alumno::create($datos->post());
+
+        return redirect('/alumnos');
+    }
+
+    function eliminar($id){
+
+    $alumno = Alumno::find($id);
+    $alumno ->delete();
+    return redirect('/alumnos');
+
+    }
+
+    function editar($id){
+
+    $alumno = Alumno::find($id);
+    return view('alumno.editar', compact('alumno'));
+    }
+
+    function actualizar(Request $datos, $id){
+    
+        $alumno = Alumno::find($id);
+        $alumno->n_control = $datos->input('n_control');
+        $alumno->nombre = $datos->input('nombre');
+        $alumno->edad = $datos->input('edad');
+        $alumno->fecha_nacimiento = $datos->input('fecha_nacimiento');
+        $alumno->domicilio = $datos->input('domicilio');
+        $alumno->telefono = $datos->input('telefono');
+        $alumno->save();
+
+        return redirect('/alumnos');
+
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlumnoController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'home']);
+Route::get('/alumnos', [AlumnoController::class, 'consultar']);
+Route::get('/alumno/registrar', [AlumnoController::class, 'registrar']);
+Route::post('/alumno/guardar', [AlumnoController::class, 'guardar']);
+Route::get('/alumno/eliminar/{id}', [AlumnoController::class, 'eliminar']);
+Route::get('/alumno/editar/{id}', [AlumnoController::class, 'editar']);
+Route::post('/alumno/actualizar/{id}', [AlumnoController::class, 'actualizar']);
